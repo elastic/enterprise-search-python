c1a17f84f90e3bc76e0da302623c667bc9e76b74
Seth Michael Larson <seth.larson@elastic.co>
2021-02-25 16:32:11 -0600

    Add support for Workplace Search OAuth

--- tests/client/workplace_search/test_workplace_search.py
+++ tests/client/workplace_search/test_workplace_search.py
@@ -20,10 +20,16 @@ import datetime
 import pytest
 from dateutil import tz
 
-from elastic_enterprise_search import NotFoundError, WorkplaceSearch
+from elastic_enterprise_search import NotFoundError, UnauthorizedError, WorkplaceSearch
 
 access_token = "b6e5d30d7e5248533b5a8f5362e16853e2fc32826bc940aa32bf3ff1f1748f9b"
 content_source_id = "5f7e1407678c1d8435a949a8"
+
+client_id = "1f87f8f6df473c06af79f88d1747afcb92e530295fad0fde340487673bec6ca6"
+client_secret = "d26a2c9aaa5870e8d6bdf8169aaf21ce2d66ec2e0180ffc34a0390d254135311"
+redirect_uri = "http://localhost:8000"
+code = "7186fec34911a182606d2ab7fc36ea0ed4b8c32fef9929235cd80294422204ca"
+refresh_token = "8be8a32c22f98a28d59cdd9d2c2028c97fa6367b77a1a41cc27f2264038ee8f3"
 
 
 @pytest.fixture()
@@ -73,3 +79,61 @@ def test_index_documents_content_source_not_found(workplace_search):
         )
     assert e.value.status == 404
     assert e.value.message == ""
+
+
+@pytest.mark.vcr()
+def test_oauth_exchange_for_access_token_code(workplace_search):
+    resp = workplace_search.oauth_exchange_for_access_token(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        code="c6424958616f102ce4e8b9e776ac8547aa06c1a603a27970547648080c43abb9",
+    )
+
+    assert resp.status == 200
+    assert resp == {
+        "access_token": "00a456134c1964a0a9e82dff3ff93d8a20e9071a51f75b2bce18dde12908eb5d",
+        "expires_in": 7200,
+        "refresh_token": "57297c5f3a7fdf9dd63d03910c49c231d869e55e2e5934835c1ffa89c3c3b704",
+        "scope": "search",
+        "token_type": "Bearer",
+    }
+
+
+@pytest.mark.vcr()
+def test_oauth_exchange_for_access_token_refresh_token(workplace_search):
+    resp = workplace_search.oauth_exchange_for_access_token(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        refresh_token="57297c5f3a7fdf9dd63d03910c49c231d869e55e2e5934835c1ffa89c3c3b704",
+    )
+
+    assert resp.status == 200
+    assert resp == {
+        "access_token": "494c72a1acaab2ac1dcf06882d874f5e54ce50c82d6b7183374597c2aceaddd6",
+        "expires_in": 7200,
+        "refresh_token": "43fb836d0600ce7a6e18087d4a674277493ec7be03b88756fe531b266db997f4",
+        "scope": "search",
+        "token_type": "Bearer",
+    }
+
+
+@pytest.mark.vcr()
+def test_oauth_exchange_for_access_token_invalid_grant(workplace_search):
+    # The 'code' parameter has already been used for this example
+    with pytest.raises(UnauthorizedError) as e:
+        workplace_search.oauth_exchange_for_access_token(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            code="7186fec34911a182606d2ab7fc36ea0ed4b8c32fef9929235cd80294422204ca",
+        )
+
+    assert e.value.status == 401
+    assert e.value.message == {
+        "error": "invalid_grant",
+        "error_description": "The provided authorization grant is invalid, expired, "
+        "revoked, does not match the redirection URI used in the "
+        "authorization request, or was issued to another client.",
+    }
