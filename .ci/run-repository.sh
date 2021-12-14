faaee15eb9ef729e14e745a7f3682c6be52e4634
Seth Michael Larson <seth.larson@elastic.co>
2021-12-14 14:25:43 -0600

    Add tests against 7.16-SNAPSHOT, Python 3.10

--- .ci/run-repository.sh
+++ .ci/run-repository.sh
@@ -12,7 +12,28 @@ script_path=$(dirname $(realpath -s $0))
 source $script_path/functions/imports.sh
 set -euo pipefail
 
-set -e
+echo -e "\033[34;1mINFO:\033[0m VERSION: ${STACK_VERSION}\033[0m"
+echo -e "\033[34;1mINFO:\033[0m TEST_SUITE: ${TEST_SUITE}\033[0m"
+echo -e "\033[34;1mINFO:\033[0m RUNSCRIPTS: ${RUNSCRIPTS}\033[0m"
+echo -e "\033[34;1mINFO:\033[0m URL: ${elasticsearch_url}\033[0m"
+
+echo -e "\033[34;1mINFO:\033[0m pinging Elasticsearch ..\033[0m"
+curl --insecure --fail $external_elasticsearch_url/_cluster/health?pretty
+
+# Unquote this block for 7.x branches to enable compatibility mode.
+# See: knowledgebase/jenkins-es-compatibility-mode.md
+# if [[ "$STACK_VERSION" == "8.0.0-SNAPSHOT" && -z "${ELASTIC_CLIENT_APIVERSIONING+x}" ]]; then
+#   export STACK_VERSION="7.x-SNAPSHOT"
+#   export ELASTIC_CLIENT_APIVERSIONING="true"
+# fi
+
+if [[ "$RUNSCRIPTS" = *"enterprise-search"* ]]; then
+  enterprise_search_url="http://localhost:3002"
+  echo -e "\033[34;1mINFO:\033[0m pinging Enterprise Search ..\033[0m"
+  curl -I --fail $enterprise_search_url
+fi
+
+echo -e "\033[32;1mSUCCESS:\033[0m successfully started the ${STACK_VERSION} stack.\033[0m"
 
 echo -e "\033[34;1mINFO:\033[0m STACK_VERSION ${STACK_VERSION}\033[0m"
 echo -e "\033[34;1mINFO:\033[0m PYTHON_VERSION ${PYTHON_VERSION}\033[0m"
